801e0dbf0d34501a800c7f08f28f6e72f0fa6624
ebelair <contact@ebelair.fr>
2018-06-03 20:47:54 +0200

    Ajoute le tri par nom

--- models/product_scale_group.py
+++ models/product_scale_group.py
@@ -3,8 +3,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from openerp.osv import fields
 from openerp.osv.orm import Model
+
+_logger = logging.getLogger(__name__)
 
 
 class product_scale_group(Model):
@@ -39,7 +43,7 @@ class product_scale_group(Model):
     _defaults = {
         'active': True,
         'company_id': lambda s, cr, uid, c: s.pool.get('res.company').
-        _company_default_get(cr, uid, 'product.product', context=c),
+            _company_default_get(cr, uid, 'product.product', context=c),
     }
 
     def send_all_to_scale_create(self, cr, uid, ids, context=None):
@@ -51,3 +55,35 @@ class product_scale_group(Model):
         myself = self.browse(cr, uid, ids, context=context)
         for scale_group in myself:
             scale_group.product_ids.send_scale_write()
+
+    # Tri les articles par nom pour la balance tactile
+    # Tri selon les catégories de 281 à 980
+    # Voir le tuto sur le portail
+    def reorder_products_by_name(self, cr, uid, ids, context=None):
+        myself = self.browse(cr, uid, ids, context=context)
+        keys = [1, 281, 421, 561, 771, 876]
+        cats = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}}
+        for group in myself:
+            logging.info('group "%s"', group.name)
+            for pp in group.product_ids:
+                if 1 <= pp.scale_sequence <= 280:
+                    cats[0][pp.name] = pp
+                elif 281 <= pp.scale_sequence <= 420:
+                    cats[1][pp.name] = pp
+                elif 421 <= pp.scale_sequence <= 560:
+                    cats[2][pp.name] = pp
+                elif 561 <= pp.scale_sequence <= 770:
+                    cats[3][pp.name] = pp
+                elif 771 <= pp.scale_sequence <= 875:
+                    cats[4][pp.name] = pp
+                elif 876 <= pp.scale_sequence <= 980:
+                    cats[5][pp.name] = pp
+
+            # Sort
+            for i, cat in cats.items():
+                logging.info('Sort cat %s', i)
+                # sorted(cat.keys(), key=unicode.lower)
+                for n in sorted(cat):
+                    logging.info('--- %s : %s => %s', n, cat[n].scale_sequence, keys[i])
+                    cat[n].write({'scale_sequence': keys[i]})
+                    keys[i] += 1
